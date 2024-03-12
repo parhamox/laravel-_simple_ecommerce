1c7a3a1b121a5982e20bd872747bd6e085afcc75
unknown <mahmoodip2@gmail.com>
2024-03-12 09:25:43 +0330

    after adding role permissions

---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,32 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\Permission;
+use Illuminate\Database\Seeder;
+
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $superAdmin = \Spatie\Permission\Models\Role::firstOrCreate(['name' => 'SuperAdmin']);
+        $admin = \Spatie\Permission\Models\Role::firstOrCreate(['name' => 'Admin']);
+        $user = \Spatie\Permission\Models\Role::firstOrCreate(['name' => 'User']);
+
+        // Assign Super Admin role and admin permissions
+        $AddPruductPermission = \Spatie\Permission\Models\Permission::firstOrCreate(['name' => 'add-pruduct']);
+        $EditPruductPermission = \Spatie\Permission\Models\Permission::firstOrCreate(['name' => 'edit-pruduct']);
+        $DeletePruductPermission = \Spatie\Permission\Models\Permission::firstOrCreate(['name' => 'delete-pruduct']);
+        $ViewPruductPermission = \Spatie\Permission\Models\Permission::firstOrCreate(['name' => 'view-pruduct']);
+        $AddAdminPermission = \Spatie\Permission\Models\Permission::firstOrCreate(['name' => 'add-admin']);
+        $EditAdminPermission = \Spatie\Permission\Models\Permission::firstOrCreate(['name' => 'edit-admin']);
+        $DeleteAdminPermission = \Spatie\Permission\Models\Permission::firstOrCreate(['name' => 'delete-admin']);
+        $BuyProductPermission = \Spatie\Permission\Models\Permission::firstOrCreate(['name' =>'buy-product']);
+
+        $superAdmin->givePermissionTo([$AddPruductPermission, $EditPruductPermission, $DeletePruductPermission ,$AddAdminPermission,$EditAdminPermission,$DeleteAdminPermission ]);
+        $admin->givePermissionTo([$AddAdminPermission, $EditAdminPermission, $DeleteAdminPermission]);
+        $user->givePermissionTo([$ViewPruductPermission , $BuyProductPermission]);
+
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $superAdmin = User::create([
+            'name' => 'Super Admin',
+            'email' => 'SuperAdmin@example.com',
+            'password' => bcrypt('Password'),
+        ]);
+        $superAdmin->assignRole('SuperAdmin');
+
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'Admin@example.com',
+            'password' => bcrypt('Password'),
+        ]);
+        $admin->assignRole('Admin');
+
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'User@example.com',
+            'password' => bcrypt('Password'),
+        ]);
+        $user->assignRole('User');
+    }
+}
